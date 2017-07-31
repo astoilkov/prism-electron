f6b94c1e0bb3ac3e0f11f612d1345ac6e3e80906
astoilkov <antonio.stoilkov@gmail.com>
2017-07-31 13:56:15 +0300

    initial implementation

---
+++ index.js
@@ -0,0 +1,33 @@
+const Prism = require('./dist/prism-core.min');
+const languages = require('./dist/languages');
+const dependencies = require('./dist/dependencies');
+
+let loadedLanguages = {};
+
+function ensureLanguageLoaded(language) {
+  if (loadedLanguages[language]) {
+    return;
+  }
+
+  loadedLanguages[language] = true;
+  loadedLanguages[languages[language]] = true;
+
+  dependencies[languages[language]].forEach(dependency => {
+    loadedLanguages[dependency] = true;
+    require(`./dist/prism-${dependency}.min.js`);
+  });
+}
+
+module.exports = {
+  highlight(code, language) {
+    ensureLanguageLoaded(language);
+
+    return Prism.highlight(code, Prism.languages[language]);
+  },
+
+  tokenize(code, language) {
+    ensureLanguageLoaded(language);
+
+    return Prism.tokenize(code, Prism.languages[language]);
+  }
+};
